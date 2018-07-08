893b02ccadee5a23cda38c75bb3e61a04fd33a7c
vinhandroid <33976100+vinhandroid@users.noreply.github.com>
2018-07-08 16:50:58 +0700

    add enity framwork  core frist for data

---
+++ TeduShop.Data/Infrastructure/Disposable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Data.Infrastructure
+{
+    //tu dong hiu cac phuong thuc
+    public class Disposable : IDisposable
+    {
+        private bool isDisposed;
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        private void Dispose(bool disposting)
+        {
+            if(!isDisposed && disposting)
+            {
+                DisposeCore();
+            }
+        }
+        //Overide this to dispose custom object
+        protected virtual void DisposeCore()
+        {
+
+        }
+
+
+
+    }
+}
